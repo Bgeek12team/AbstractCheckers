8b5d73ee9eb4b58d9672f2a27364e1efb75216fa
ghimik <bolit006.br0m@gmail.com>
2024-03-25 22:12:24 +0300

    микропэнси

--- CheckersClasslib/Cell.cs
+++ CheckersClasslib/Cell.cs
@@ -1,7 +1,8 @@
 ﻿namespace CheckersClasslib;
 
-public abstract class Cell
+public abstract class Cell(Teams team)
 {
+    public Teams Team { get; set; } = team;
     public abstract void HandleInMovement(Move move, Board board);
     public abstract void HandleOutMovement(Move move, Board board);
 }
--- CheckersClasslib/Figure.cs
+++ CheckersClasslib/Figure.cs
@@ -1,7 +1,8 @@
 ﻿namespace CheckersClasslib;
 
-public abstract class Figure
+public abstract class Figure(Teams team)
 {
+    public Teams Team { get; set; } = team;
     public int X { get; set; }
     public int Y { get; set; }
 
